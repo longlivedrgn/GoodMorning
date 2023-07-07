d3d314c2600dcfe618932dfd8127336e23f34ebc
SunnnySong <tjsghktjswls@naver.com>
2023-07-07 15:21:25 +0900

    start: GoodMorning 프로젝트 생성
    - GoodMorning 프로젝트 생성
    - gitignore 파일 추가

---
+++ GoodMorning/GoodMorning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodMorning
+//
+//  Created by Sunny on 2023/07/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
