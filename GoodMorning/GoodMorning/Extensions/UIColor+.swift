2ddd400769f4099a81dcff9f479aabcfd61df067
SunnnySong <tjsghktjswls@naver.com>
2023-07-16 16:53:29 +0900

    start: app의 메인 색상 추가

---
+++ GoodMorning/GoodMorning/Extensions/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  GoodMorning
+//
+//  Created by Sunny on 2023/07/16.
+//
+
+import UIKit
+
+extension UIColor {
+    class func design(_ color: Palette) -> UIColor? {
+        return UIColor(named: color.rawValue)
+    }
+}
---
+++ GoodMorning/GoodMorning/Resources/Designs/Colors/Palette.swift
@@ -0,0 +1,11 @@
+//
+//  Palette.swift
+//  GoodMorning
+//
+//  Created by Sunny on 2023/07/16.
+//
+
+enum Palette: String {
+    case mainBackground
+    case checkBox
+}
