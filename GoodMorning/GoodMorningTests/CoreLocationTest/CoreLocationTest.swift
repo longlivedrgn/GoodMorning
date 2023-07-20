e620df0af77f3dc9bff398df86d17480077f286c
longlivedrgn <kimye9808@naver.com>
2023-07-20 16:50:29 +0900

    test: Core Location error 테스트 코드 작성

--- GoodMorning/GoodMorningTests/CoreLocationTest/CoreLocationTest.swift
+++ GoodMorning/GoodMorningTests/CoreLocationTest/CoreLocationTest.swift
@@ -17,13 +17,11 @@ final class CoreLocationTest: XCTestCase {
     override func setUpWithError() throws {
         super.setUp()
         locationFetcher = MockLocationFetcher()
-        locationFetcher.callBack = {
-            return (CLLocation(latitude: 10, longitude: 10), nil)
-        }
         sut = LocationManager(locationFetcher: locationFetcher)
     }
 
     override func tearDownWithError() throws {
+        super.tearDown()
         sut = nil
         locationFetcher = nil
     }
@@ -35,9 +33,24 @@ final class CoreLocationTest: XCTestCase {
         sut.fetchCurrentLocation { location, error in
             resultLocation = location
         }
+        locationFetcher.locationCallBackValue = CLLocation(latitude: 10, longitude: 10)
+        locationFetcher.locationErrorCallBackValue = nil
         sut.updateLocation()
 
         XCTAssertEqual(resultLocation?.coordinate.latitude, expectationLocation.coordinate.latitude)
+    }
+
+    func test_네트워크_오류로_로케이션을_받아올_수_없을_때_에러를_던지는지_확인() {
+        var coreLocationError: Error?
+
+        sut.fetchCurrentLocation { location, error in
+            coreLocationError = error
+        }
+
+        locationFetcher.locationErrorCallBackValue = LocationError.canNotBeLocated
+        sut.updateLocation()
+
+        XCTAssertNotNil(coreLocationError)
     }
 
 }
