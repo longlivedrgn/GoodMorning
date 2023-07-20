79beb6c1f9c1f7bc0064f31a23ea05ecc2c65b30
longlivedrgn <kimye9808@naver.com>
2023-07-20 20:52:07 +0900

    feat: OpenWeatherAPI 열거형 정의

---
+++ GoodMorning/GoodMorning/API/OpenWeatherAPI.swift
@@ -0,0 +1,46 @@
+//
+//  OpenWeatherAPI.swift
+//  GoodMorning
+//
+//  Created by Miro on 2023/07/20.
+//
+
+import Foundation
+
+// 아래는 apiKey가 정의되었다고 가정
+extension Bundle {
+    static var openWeatherAPIKey = "key"
+}
+
+enum OpenWeatherAPI {
+
+    case currentWeather(Coordinate)
+
+    static var baseURL: String {
+        return "https://api.openweathermap.org"
+    }
+
+    var path: String {
+        switch self {
+        case .currentWeather:
+            return "/data/2.5/weather"
+        }
+    }
+
+    var query: [URLQueryItem] {
+        var queryItem = [URLQueryItem]()
+        let keyParameter = URLQueryItem(name: "appid", value: Bundle.openWeatherAPIKey)
+        queryItem.append(keyParameter)
+
+        switch self {
+        case .currentWeather(let coordinate):
+            let latitudeParameter = URLQueryItem(name: "lat", value: coordinate.latitude)
+            let longitudeParameter = URLQueryItem(name: "lon", value: coordinate.longitude)
+            let unitParameter = URLQueryItem(name: "units", value: "metric")
+            queryItem.append(contentsOf: [latitudeParameter, longitudeParameter, unitParameter])
+        }
+
+        return queryItem
+    }
+
+}
