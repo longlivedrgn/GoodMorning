dc71170a547f04cb88189cd43da470ffe31db6ea
longlivedrgn <kimye9808@naver.com>
2023-08-04 14:09:06 +0900

    feat: Item, Section 타입 정의하기

--- GoodMorning/GoodMorning/ViewController.swift
+++ GoodMorning/GoodMorning/ViewController.swift
@@ -8,6 +8,28 @@
 import UIKit
 import CoreLocation
 
+private enum Section: Hashable {
+
+    case main
+
+}
+
+private struct Item: Hashable {
+
+    let iconImage: UIImage
+    let description: String
+    let isChecked: Bool
+    let identifier = UUID()
+
+    static let allItems = [
+        Item(iconImage: UIImage(systemName: "headphones")!, description: "Swift 공부하기", isChecked: false),
+        Item(iconImage: UIImage(systemName: "headphones")!, description: "물 한잔 마시기", isChecked: false),
+        Item(iconImage: UIImage(systemName: "headphones")!, description: "집 청소하기", isChecked: false),
+        Item(iconImage: UIImage(systemName: "headphones")!, description: "야곰한테 전화하기", isChecked: false)
+    ]
+
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
