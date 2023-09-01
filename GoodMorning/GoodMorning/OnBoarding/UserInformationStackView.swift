41179ecd2d8f50d2259701f9484ed11dfd6374db
longlivedrgn <kimye9808@naver.com>
2023-09-01 22:59:18 +0900

    chore: UserInformationStackView - initializer 수정

--- GoodMorning/GoodMorning/OnBoarding/UserInformationStackView.swift
+++ GoodMorning/GoodMorning/OnBoarding/UserInformationStackView.swift
@@ -11,18 +11,17 @@ final class UserInformationStackView: UIStackView {
 
     private(set) var titleLabel: PretendardLabel
 
-    override init(frame: CGRect) {
-        self.titleLabel = PretendardLabel(text: "", size: 10, weight: .medium)
-        super.init(frame: frame)
+    init(title: String, subView: UIView) {
+        self.titleLabel = PretendardLabel(text: title, alignment: .left, size: 20, weight: .bold)
+        super.init(frame: .zero)
+        self.configureStackView(with: subView)
     }
 
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    convenience init(title: String, subView: UIView) {
-        self.init(frame: .zero)
-        self.titleLabel = PretendardLabel(text: title, alignment: .left, size: 20, weight: .bold)
+    private func configureStackView(with subView: UIView) {
         self.addArrangedSubviews([self.titleLabel, subView])
         self.axis = .vertical
         self.spacing = 10
