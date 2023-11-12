6e92d0e02d3404f0f2ae9cf857583faf5d9e6f2f
SunnnySong <tjsghktjswls@naver.com>
2023-11-13 03:54:43 +0900

    feat: ToDoItem에 title 속성 추가

--- GoodMorning/GoodMorning/HomeScene/Models/ToDoItem.swift
+++ GoodMorning/GoodMorning/HomeScene/Models/ToDoItem.swift
@@ -10,26 +10,31 @@ import UIKit
 struct ToDoItem: Hashable {
 
     var iconImage: String?
-    var description: String
+    var title: String
+    var description: String?
     var isChecked: Bool
     var priority: Priority
     let identifier: UUID = UUID()
 
     static let allItems = [
         ToDoItem(
-            iconImage: "ㅎ",
+            iconImage: "ㅎ", 
+            title: "zz",
             description: "Swift 공부하기", isChecked: false, priority: .high
         ),
         ToDoItem(
             iconImage: "ㅎ",
+            title: "zz",
             description: "물 한잔 마시기", isChecked: false, priority: .high
         ),
         ToDoItem(
             iconImage: "ㅎ",
+            title: "zz",
             description: "집 청소하기", isChecked: false, priority: .medium
         ),
         ToDoItem(
             iconImage: "ㅎ",
+            title: "zz",
             description: "야곰한테 전화하기", isChecked: false, priority: .low
         )
     ]
