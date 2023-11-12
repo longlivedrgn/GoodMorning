03f7c2d31e29238ac3c709d3d32a0b734b4f6058
SunnnySong <tjsghktjswls@naver.com>
2023-11-13 03:13:46 +0900

    feat: ToDoModalViewModel 파일 생성

---
+++ GoodMorning/GoodMorning/TODOModalScene/ToDoModalViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoModalViewModel.swift
+//  GoodMorning
+//
+//  Created by Sunny on 11/13/23.
+//
+
+final class ToDoModalViewModel {
+
+    private let todoListUseCase: ToDoListUseCase
+
+    init(
+
+        todoListUseCase: ToDoListUseCase
+    ) {
+        self.todoListUseCase = todoListUseCase
+    }
+
+
+}
